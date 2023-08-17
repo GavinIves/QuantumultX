4ce7998587b7b539240ea9c0be992e12a7b5aee4
IVES <38970877+GavinIves@users.noreply.github.com>
2023-08-17 19:07:11 +0800

    Update A_gowork.js

--- JS/A_gowork.js
+++ JS/A_gowork.js
@@ -1,14 +1,14 @@
 let body = $request.body
 let url=$request.url
-if (url.includes('employee/location.serv')) {
+if (url.includes("employee/location.serv")) {
 body=JSON.parse(body)
-body['body']['gpslat']=31.360688169596311
-body['body']['gpslng']=120.4259554048311
+body["body"]["gpslat"]=31.360688169596311
+body["body"]["gpslng"]=120.4259554048311
 body=JSON.stringify(body)
-} else if (currentUrl.includes('employee/checkin.serv')) {
+} else if (currentUrl.includes("employee/checkin.serv")) {
 body=JSON.parse(body)
-body['body']['gpslat']="31.360675"
-body['body']['gpslng']="120.425975"
+body["body"]["gpslat"]="31.360675"
+body["body"]["gpslng"]="120.425975"
 body=JSON.stringify(body)
 }
 $done({body})
