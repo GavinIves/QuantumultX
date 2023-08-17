b4fff336cee56210ffd0d79ac70bbd5cabf21b14
IVES <38970877+GavinIves@users.noreply.github.com>
2023-08-17 20:09:32 +0800

    Update A_gowork.js

--- JS/A_gowork.js
+++ JS/A_gowork.js
@@ -5,7 +5,7 @@ body=JSON.parse(body)
 body["body"]["gpslat"]=31.360688169596311
 body["body"]["gpslng"]=120.4259554048311
 body=JSON.stringify(body)
-} else if (currentUrl.includes("employee/checkin.serv")) {
+} else if (url.includes("employee/checkin.serv")) {
 body=JSON.parse(body)
 body["body"]["gpslat"]="31.360675"
 body["body"]["gpslng"]="120.425975"
