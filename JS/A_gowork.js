c6977c065a094efb2a13738408f46a96a40f645d
IVES <38970877+GavinIves@users.noreply.github.com>
2023-08-21 11:03:23 +0800

    Update A_gowork.js

--- JS/A_gowork.js
+++ JS/A_gowork.js
@@ -1,14 +1,15 @@
 let body = $request.body
 let url=$request.url
+const randomFloat = Math.random()*0.01;
 if (url.includes("employee/location.serv")) {
 body=JSON.parse(body)
-body["body"]["gpslat"]=31.360688169596311
-body["body"]["gpslng"]=120.4259554048311
+body["body"]["gpslat"]=Number((31.360688169596311+randomFloat).toFixed(15))
+body["body"]["gpslng"]=Number((120.4259554048311+randomFloat).toFixed(15))
 body=JSON.stringify(body)
 } else if (url.includes("employee/checkin.serv")) {
 body=JSON.parse(body)
-body["body"]["gpslat"]="31.360675"
-body["body"]["gpslng"]="120.425975"
+body["body"]["gpslat"]=(31.360688169596311+randomFloat).toFixed(6)
+body["body"]["gpslng"]=(120.4259554048311+randomFloat).toFixed(6)
 body=JSON.stringify(body)
 }
 $done({body})
